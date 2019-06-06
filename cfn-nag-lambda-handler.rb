6beffabd136c6a8b319316f1455f63c7be95e5e4
Jesse R. Adams <jesse.adams@stelligent.com>
2019-06-06 14:22:42 -0700

    Updating lambda

--- cfn-nag-lambda-handler.rb
+++ cfn-nag-lambda-handler.rb
@@ -5,26 +5,18 @@ require 'base64'
 module LambdaFunctions
   class Handler
     def self.process(event:,context:)
-        #template_body = JSON.parse(event['template_body'])
-        #template_body = JSON.pretty_generate(template_body)
         template_body = Base64.decode64(event['template_body'])
         aggregate_results = []
         config = CfnNagConfig.new
         cfn_nag = CfnNag.new(config: config)
 
-        aggregate_results << scan_file(cfn_nag, template_body)
-
-        cfn_nag.render_results(aggregate_results: aggregate_results,
-                               output_format: 'json')
+        scan_file(cfn_nag, template_body)
     end
 
     def self.scan_file(cfn_nag, template_body)
       audit_result = cfn_nag.audit(cloudformation_string: template_body)
-
-      {
-        filename: 'lambda_payload',
-        file_results: audit_result
-      }
+      audit_result[:violations] = audit_result[:violations].map(&:to_h)
+      audit_result
     end
   end
 end
