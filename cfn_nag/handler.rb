f47a8a7080ee735c5d5d998a93730554081666ba
Jesse R. Adams <jesse.adams@stelligent.com>
2019-06-24 13:31:18 -0700

    Adding params

--- cfn_nag/handler.rb
+++ cfn_nag/handler.rb
@@ -2,6 +2,7 @@
 require 'cfn-nag'
 require 'base64'
 require 'json'
+require 'rbnacl'
 
 module LambdaFunctions
   class Handler
@@ -14,9 +15,7 @@ module LambdaFunctions
         cfn_nag = CfnNag.new(config: config)
 
         result_string = scan_file(cfn_nag, template_body)
-
-        secure = event['resource'] == '/scan_secure' ? true : false
-        response = build_response(result_string, secure)
+        response = build_response(result_string, event)
 
         {
           "body" => response
@@ -32,12 +31,41 @@ module LambdaFunctions
       }
     end
 
-    def self.build_response(result_string, secure = false)
-      if secure
-        "Not implemented yet"
-      else
-        result_string
+    def self.build_response(result_string, event)
+      params = event['queryStringParameters']
+      result_string =
+        if event['resource'] == '/scan_secure'
+          'Not implemented yet'
+        else
+          result_string
+        end
+
+      return result_string unless param_truthy?(params['return_template']) ||
+                                  param_truthy?(params['return_rules'])
+
+      response = {
+        'result' => result_string
+      }
+
+      if param_truthy?(params['return_template'])
+        response['template'] = event['body']['template_body']
       end
+
+      if param_truthy?(params['return_rules'])
+        response['rules'] = get_rules
+      end
+
+      response
+    end
+
+    def self.param_truthy?(param)
+      param.to_s.casecmp('true').zero?
+    end
+
+    def self.get_rules
+      custom_rule_loader = CustomRuleLoader.new
+      rules = custom_rule_loader.rule_definitions.rules
+      rules.to_json
     end
 
     def self.scan_file(cfn_nag, template_body)
