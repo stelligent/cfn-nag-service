3ef376d364c1218e566a9a28a1d22eb97f665c08
Jesse R. Adams <jesse.adams@stelligent.com>
2019-06-25 10:28:20 -0700

    Remove interactive mode

--- scripts/extract_libsodium.sh
+++ scripts/extract_libsodium.sh
@@ -4,6 +4,6 @@ set -e
 
 rm -f libsodium.zip
 rm -f lib/libsodium*
-docker run -v `pwd`:/var/task -i -t lambci/lambda:build-ruby2.5 ./scripts/download_libsodium.sh
+docker run -v `pwd`:/var/task -t lambci/lambda:build-ruby2.5 ./scripts/download_libsodium.sh
 zip libsodium.zip lib/ -r
 aws s3 cp libsodium.zip s3://stelligent-cfn-nag-service/libsodium.zip
