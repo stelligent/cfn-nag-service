d99a8807ec0b6ac5d7d520c39858ee8aa51978a4
Jesse R. Adams <jesse.adams@stelligent.com>
2019-06-25 11:30:35 -0700

    Refactoring secure scan

--- cfn_nag/handler.rb
+++ cfn_nag/handler.rb
@@ -33,26 +33,25 @@ module LambdaFunctions
 
     def self.build_response(result_string, event)
       params = event['queryStringParameters']
-      result_string =
-        if event['resource'] == '/scan_secure'
-          signing_key = RbNaCl::SigningKey.generate
-          signature = signing_key.sign(result_string.to_json.to_s)
-          Base64.encode64 signature
-        else
-          result_string
-        end
-
       return_template = param_truthy?(params, 'return_template')
       return_rules = param_truthy?(params, 'return_rules')
-      return result_string unless return_template || return_rules
+      sign_request = event['resource'] == '/scan_secure'
+      return result_string unless return_template || return_rules || sign_request
 
       response = {
-        'result' => result_string
+        'results' => result_string
       }
 
+      response['signature'] = sign_results(result_string) if sign_request
       response['template'] = JSON.parse(event['body'])['template_body'] if return_template
       response['rules'] = get_rules if return_rules
       response
+    end
+
+    def self.sign_results(result_string)
+      signing_key = RbNaCl::SigningKey.generate\
+      signature = signing_key.sign(result_string.to_json.to_s)
+      Base64.encode64 signature
     end
 
     def self.param_truthy?(params, param_name)
